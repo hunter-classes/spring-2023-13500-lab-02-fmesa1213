06b76d02786ac11835563fcfeafd3e2dc15ab543
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-02-07 15:02:35 -0500

    Update valid.cpp

--- valid.cpp
+++ valid.cpp
@@ -19,18 +19,18 @@ int main ()
   
 cout << "Please enter an integer:" << endl;
   cin >> number; 
-  
-  if ((0 < number) && (number < 100)) 
-  {
-    square = number*number;
-    cout<< square <<endl;
-  }
 
-  else  
+  while (number <= 0 || number >= 100)  
   {
     cout << "Please re-enter:" << endl; 
     cin >> number;
 
-    }  
+    } 
+  
+  if ((0 < number) && (number < 100)) 
+  {
+    square = number*number;
+    cout<< "Number squared is " << square <<endl;
+  } 
   return 0;
 }
