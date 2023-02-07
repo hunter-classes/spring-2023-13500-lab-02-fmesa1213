63702d76a48f36575a9e800fc46f06fc0aff72de
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-02-07 15:06:40 -0500

    Create print-interval.cpp

---
+++ print-interval.cpp
@@ -0,0 +1,10 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: 
+Assignment: Lab2B
+Wrote a program that asks the user to input two integers L and U 
+(representing the lower and upper limits of the interval), 
+and print out all integers in the range L ≤ i < U separated by spaces. 
+*/
+
